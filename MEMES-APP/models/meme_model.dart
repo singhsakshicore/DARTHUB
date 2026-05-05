e692f562938f35c35fb39340645bc4237a4dc08e
SAKSHI SINGH <singhsakshi9494@gmail.com>
2026-05-05 15:06:08 +0530

    Add files via upload

---
+++ MEMES-APP/models/meme_model.dart
@@ -0,0 +1,51 @@
+class Meme {
+  final String postLink;
+  final String subreddit;
+  final String title;
+  final String url;
+  final bool nsfw;
+  final bool spoiler;
+  final String author;
+  final int ups;
+  final List<String> preview;
+
+  Meme({
+    required this.postLink,
+    required this.subreddit,
+    required this.title,
+    required this.url,
+    required this.nsfw,
+    required this.spoiler,
+    required this.author,
+    required this.ups,
+    required this.preview,
+  });
+
+  factory Meme.fromJson(Map<String, dynamic> json) {
+    return Meme(
+      postLink: json['postLink'],
+      subreddit: json['subreddit'],
+      title: json['title'],
+      url: json['url'],
+      nsfw: json['nsfw'],
+      spoiler: json['spoiler'],
+      author: json['author'],
+      ups: json['ups'],
+      preview: List<String>.from(json['preview']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'postLink': postLink,
+      'subreddit': subreddit,
+      'title': title,
+      'url': url,
+      'nsfw': nsfw,
+      'spoiler': spoiler,
+      'author': author,
+      'ups': ups,
+      'preview': preview,
+    };
+  }
+}
