9e1be63ea46ee7da54a503c21d972a966b40858a
SAKSHI SINGH <singhsakshi9494@gmail.com>
2026-05-18 08:55:12 +0530

    Add files via upload

---
+++ REST API CALL/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/screen/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
